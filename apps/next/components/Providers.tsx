848fa7704fa694b5b12c7fc942e2c49540320afa
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-05-21 14:50:39 +0200

    feat: add code-generator package and update imports
    - Create new package 'code-generator' in the 'packages' directory
    - Move 'CodeGenerator' component and context to the package
    - Add necessasry exports and path aliases
    - Update imports in the Next.js app

--- apps/next/components/Providers.tsx
+++ apps/next/components/Providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { DndContext } from "@dnd-kit/core";
-import { CodeGeneratorProvider } from "./CodeGenerator/context";
+import { CodeGeneratorProvider } from "@code-generator/CodeGenerator/context";
 
 export const Providers: React.FC<React.PropsWithChildren<{}>> = ({
   children,
