15dfa14ae3e69e57fe510d87fbaa6649187c3061
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-05-24 22:05:44 +0200

    refactor(code-generator): move 'output' field to context and rename

--- packages/code-generator/src/CodeGenerator/context.tsx
+++ packages/code-generator/src/CodeGenerator/context.tsx
@@ -1,26 +1,41 @@
-import { PropsWithChildren, createContext, useContext, useState } from "react";
-import { FunctionInfo } from "@repo/parser";
+import { FunctionInfo, VariableInfo } from "@repo/parser";
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { generateCode } from "./functions";
 
 interface Context {
   functions: FunctionInfo[];
   setFunctions: (functions: FunctionInfo[]) => void;
+  code: string;
 }
 
 const CodeGeneratorContext = createContext<Context>({
   functions: [],
   setFunctions: () => {},
+  code: "",
 });
 
 export const CodeGeneratorProvider: React.FC<PropsWithChildren> = ({
   children,
 }) => {
   const [functions, setFunctions] = useState<FunctionInfo[]>([]);
+  const [code, setCode] = useState<string>("");
+
+  useEffect(() => {
+    setCode(generateCode(functions));
+  }, [functions, setFunctions]);
 
   return (
     <CodeGeneratorContext.Provider
       value={{
         functions,
         setFunctions,
+        code,
       }}
     >
       {children}
