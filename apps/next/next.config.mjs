56990871d39a290114461e3847d3f1b4edaa1df3
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-07-21 19:43:39 +0200

    chore: Update webpack configuration to disable fallback for non-server builds

--- apps/next/next.config.mjs
+++ apps/next/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
