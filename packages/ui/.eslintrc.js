2c9565b77694e5649afe71ea322a73925e94a2f4
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-07-23 23:21:49 +0200

    Refactor ESLint and TypeScript configs

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -3,7 +3,19 @@ module.exports = {
   root: true,
   extends: ["@repo/eslint-config/react-internal.js"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
   rules: {
     "no-redeclare": "off",
   },
+  overrides: [
+    {
+      files: ["**/*.test.ts", "**/*.test.tsx"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
