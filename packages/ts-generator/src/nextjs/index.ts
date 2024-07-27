bd0398e9172fe5634962024b29928fe58f3a4b49
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-07-27 18:55:35 +0200

    remove ts-codegenerator from repo and add as dependency

--- packages/ts-generator/src/nextjs/index.ts
+++
@@ -1,3 +0,0 @@
-export type { NextCodebaseInfo, ServerActionInfo } from "./types";
-export { scanNextjsCodebase, analyzeNextjsSourceFiles } from "./scanner";
-export { generateServerAction } from "./generator";
