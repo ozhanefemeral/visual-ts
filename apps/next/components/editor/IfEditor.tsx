b134c09beb6fc98d41b93a3d8f8505a206c85b25
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-08-03 09:58:49 +0200

    feat: Add FunctionCombobox to IfEditor for selecting functions
    - Add FunctionCombobox component to IfEditor
    - Implement onSelect callback to add selected function inside the selected block
    - Pass `resetAfterSelect` prop to FunctionCombobox

--- apps/next/components/editor/IfEditor.tsx
+++ apps/next/components/editor/IfEditor.tsx
@@ -1,28 +1,26 @@
+import { useBlockEditor } from "@/contexts/BlockEditorContext";
 import { useCodeGenerator } from "@/contexts/CodeGeneratorContext";
-import { IfBlock } from "@ozhanefe/ts-codegenerator";
-import { Button } from "@ui/button";
+import { FunctionInfo, IfBlock } from "@ozhanefe/ts-codegenerator";
 import { Input } from "@ui/input";
-import React, { useState } from "react";
+import React from "react";
+import { FunctionCombobox } from "../FunctionCombobox";
+import { Separator } from "@ui/separator";
 
 export const IfEditor: React.FC<{ block: IfBlock }> = ({ block }) => {
   const condition = block.condition;
-  const [conditionInput, setConditionInput] = useState(condition);
   const { setState } = useCodeGenerator();
+  const { createFunctionInside } = useBlockEditor();
 
   const handleConditionChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    setConditionInput(event.target.value);
-  };
-
-  const handleConditionSubmit = () => {
     setState((state) => {
       if (state.blocks.map((b) => b.index).includes(block.index)) {
         const blocks = state.blocks.map((b) => {
           if (b.index === block.index) {
             return {
               ...b,
-              condition: conditionInput,
+              condition: event.target.value,
             };
           }
           return b;
@@ -36,10 +34,18 @@ export const IfEditor: React.FC<{ block: IfBlock }> = ({ block }) => {
       return state;
     });
   };
+
+  const addFunction = (func: FunctionInfo) => {
+    createFunctionInside(func);
+  };
+
   return (
     <React.Fragment>
-      <Input value={conditionInput} onChange={handleConditionChange} />
-      <Button onClick={handleConditionSubmit}>Update</Button>
+      <FunctionCombobox onSelect={addFunction} resetAfterSelect />
+      <Separator orientation="vertical" />
+      <div>
+        <Input defaultValue={condition} onChange={handleConditionChange} />
+      </div>
     </React.Fragment>
   );
 };
