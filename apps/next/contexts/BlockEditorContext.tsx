38beccb8b1a5a9f97cec6d2211f7b84f6d99b9a6
Özhan Efe Meral <ozhanefemeral@gmail.com>
2024-08-03 09:56:36 +0200

    feat: Add createFunctionInside method to BlockEditorContext
    - bump ts-codegenerator version

--- apps/next/contexts/BlockEditorContext.tsx
+++ apps/next/contexts/BlockEditorContext.tsx
@@ -6,26 +6,52 @@ import {
   useContext,
   useState,
 } from "react";
-import { CodeBlock } from "@ozhanefe/ts-codegenerator";
+import {
+  CodeBlock,
+  FunctionInfo,
+  createFunctionCallBlock,
+} from "@ozhanefe/ts-codegenerator";
+import { useCodeGenerator } from "./CodeGeneratorContext";
 
 interface Context {
   currentBlock: CodeBlock | null;
   setCurrentBlock: Dispatch<SetStateAction<CodeBlock | null>>;
+  createFunctionInside: (func: FunctionInfo) => void;
 }
 
 const BlockEditorContext = createContext<Context>({
   currentBlock: null,
   setCurrentBlock: () => {},
+  createFunctionInside: () => {},
 });
 
 export const BlockEditorProvider = ({ children }: PropsWithChildren<{}>) => {
+  const { setState } = useCodeGenerator();
   const [currentBlock, setCurrentBlock] = useState<CodeBlock | null>(null);
+
+  const createFunctionInside = (func: FunctionInfo) => {
+    if (!currentBlock) return;
+
+    switch (currentBlock.blockType) {
+      case "if": {
+        setState((state) => {
+          const { state: newState } = createFunctionCallBlock(
+            func,
+            state,
+            currentBlock
+          );
+          return newState;
+        });
+      }
+    }
+  };
 
   return (
     <BlockEditorContext.Provider
       value={{
         currentBlock,
         setCurrentBlock,
+        createFunctionInside,
       }}
     >
       {children}
